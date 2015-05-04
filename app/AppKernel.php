397485fc28d422441dcf53c51feaa054c1b070e7
justasp <justinas.putis@gmail.com>
2015-05-05 00:32:37 +0300

    added OrderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new OurRead\LibraryBundle\LibraryBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new OurRead\UserBundle\UserBundle(),
+            new OurRead\OrderBundle\OrderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
