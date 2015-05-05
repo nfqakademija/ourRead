c940e5148d8c470fd4bcaab097c102c0c1406057
Povilas <povilas.susinskas@gmail.com>
2015-05-05 18:33:44 +0000

    confirmation

--- src/OurRead/OurBundle/Resources/public/js/custom.js
+++ src/OurRead/OurBundle/Resources/public/js/custom.js
@@ -30,4 +30,23 @@ $(document).ready(function() {
 $('#form-add-book').submit(function() {
     return confirm("Click OK if you are sure you want to add this book");
 });
+$('.delete-action').click(function(e) {
+    e.preventDefault();
+    $.confirm({
+        confirmButton: 'Yes',
+        cancelButton: 'No!',
+        icon: 'fa fa-warning',
+        title: 'Warning!',
+        content: 'Are you sure you want to delete this book?!',
+        cancelButtonClass: 'btn-info',
+        confirmButtonClass: 'btn-danger',
+        animation: 'bottom',
+        autoClose: 'cancel|10000',
+        confirm: function(){
+            window.location = $(this).attr('href');
+        },
+        cancel: function(){
 
+        }
+    });
+});
