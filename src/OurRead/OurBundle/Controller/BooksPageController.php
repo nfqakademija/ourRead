459a963f3a9234fa6c809b541c3dbef1537d62e1
dovius <dovius.vilI@gmail.com>
2015-05-05 21:35:07 +0300

    books page

---
+++ src/OurRead/OurBundle/Controller/BooksPageController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dovius
+ * Date: 5/5/15
+ * Time: 9:06 PM
+ */
+namespace OurRead\OurBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OurRead\LibraryBundle\Entity\Book;
+use Symfony\Component\HttpFoundation\Response;
+
+class BooksPageController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('');
+    }
+
+
+}
