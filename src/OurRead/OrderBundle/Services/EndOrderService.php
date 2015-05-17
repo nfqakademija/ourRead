2736419dced283bafeea61d7db9ce5fcd634d4e7
Povilas <povilas.susinskas@gmail.com>
2015-05-17 10:07:50 +0000

    fixing duplication

--- src/OurRead/OrderBundle/Services/EndOrderService.php
+++ src/OurRead/OrderBundle/Services/EndOrderService.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: povilas
  * Date: 5/16/15
- * Time: 3:55 PM
+ * Time: 3:28 PM
  */
 
 namespace OurRead\OrderBundle\Services;
@@ -12,7 +12,7 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 use OurRead\LibraryBundle\Entity\Book;
 use OurRead\UserBundle\Entity\Users;
 
-class CancelReservationService
+class EndOrderService
 {
     private $managerRegistry;
 
@@ -21,17 +21,19 @@ class CancelReservationService
         $this->managerRegistry = $managerRegistry;
     }
 
-    public function cancelReservation(Book $book, Users $user)
+    public function endOrder(Book $book, Users $user,$orderType)
     {
         $entityManager = $this->managerRegistry->getManager();
-        $repository = $entityManager->getRepository('OrderBundle:Orders');
+        $repository = $entityManager
+            ->getRepository('OrderBundle:Orders');
         $order = $repository->createQueryBuilder('orders')
             ->where('orders.status = 0')
             ->andWhere('orders.bookId = :book_id')
             ->andWhere('orders.userId = :user_id')
-            ->andWhere('orders.orderType = 1')
+            ->andWhere('orders.orderType = :order_type')
             ->setParameter('book_id', $book->getId())
             ->setParameter('user_id', $user->getId())
+            ->setParameter('order_type', $orderType)
             ->getQuery()
             ->getSingleResult();
         $order->setStatus(1);
