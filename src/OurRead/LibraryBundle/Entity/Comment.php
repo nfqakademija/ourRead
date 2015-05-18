c76491ef0aa526cfa3f12cdc9e4e9f874491856a
Povilas <povilas.susinskas@gmail.com>
2015-05-18 09:08:41 +0000

    commenting

---
+++ src/OurRead/LibraryBundle/Entity/Comment.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: povilas
+ * Date: 5/17/15
+ * Time: 10:40 PM
+ */
+
+namespace OurRead\LibraryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+/**
+ * @ORM\Entity
+ * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
+ */
+class Comment extends BaseComment implements SignedCommentInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Thread of this comment
+     *
+     * @var Thread
+     * @ORM\ManyToOne(targetEntity="OurRead\LibraryBundle\Entity\Thread")
+     */
+    protected $thread;
+
+    /**
+
+     * @ORM\ManyToOne(targetEntity="OurRead\UserBundle\Entity\Users")
+     * @var UserInterface $author
+     */
+    protected $author;
+
+
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
+
+}
