285e8a797e2feaa7fc4c8f3aa79e4200a6d46c69
Povilas <povilas.susinskas@gmail.com>
2015-05-18 19:57:57 +0000

    tests

--- src/OurRead/OrderBundle/DataFixtures/ORM/LoadOrderData.php
+++ src/OurRead/OrderBundle/DataFixtures/ORM/LoadOrderData.php
@@ -8,7 +8,69 @@
 
 namespace OurRead\OrderBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use OurRead\OrderBundle\Entity\Orders;
 
-class LoadOrderData {
 
+class LoadOrderData  extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $order = new Orders();
+        $order->setUserId($this->getReference('user1'));
+        $order->setBookId($this->getReference('book3'));
+        $order->setStartDate(new \DateTime());
+        $order->setEndDate(new \DateTime($this->getReturnDate()));
+        $order->setOrderType(0);
+        $order->setStatus(0);
+        $order->setExtendedStatus(0);
+        $order->setConfirmStatus(0);
+        $manager->persist($order);
+
+        $order = new Orders();
+        $order->setUserId($this->getReference('user1'));
+        $order->setBookId($this->getReference('book4'));
+        $order->setStartDate(new \DateTime());
+        $order->setEndDate(new \DateTime($this->getReturnDate()));
+        $order->setOrderType(0);
+        $order->setStatus(0);
+        $order->setExtendedStatus(0);
+        $order->setConfirmStatus(0);
+        $manager->persist($order);
+
+        $manager->flush();
+
+    }
+
+    public function getOrder()
+    {
+        return 4; // the order in which fixtures will be loaded
+    }
+
+    private function getReturnDate()
+    {
+        $date = new \DateTime();
+        $dateAfterOneMonth = new \DateInterval('P1M');
+        $date->add($dateAfterOneMonth);
+
+        return $date->format('Y-m-d');
+    }
 } 
